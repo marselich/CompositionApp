1ccd7e0d30c4a83676e3772db685417a41cf5792
marselich <marsellkaliev7722@gmail.com>
2022-09-26 15:45:22 +0500

    Setup welcome fragment to main activity

--- app/src/main/java/ru/kalievmars/compositionapp/MainActivity.kt
+++ app/src/main/java/ru/kalievmars/compositionapp/MainActivity.kt
@@ -2,10 +2,14 @@ package ru.kalievmars.compositionapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.kalievmars.compositionapp.fragments.WelcomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container_view, WelcomeFragment.newInstance())
     }
 }
--- app/src/main/java/ru/kalievmars/compositionapp/fragments/WelcomeFragment.kt
+++ app/src/main/java/ru/kalievmars/compositionapp/fragments/WelcomeFragment.kt
@@ -18,16 +18,9 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class WelcomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -49,12 +42,6 @@ class WelcomeFragment : Fragment() {
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            WelcomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() = WelcomeFragment()
     }
 }
